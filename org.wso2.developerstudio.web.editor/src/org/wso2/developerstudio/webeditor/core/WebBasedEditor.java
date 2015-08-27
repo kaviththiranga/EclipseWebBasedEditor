ad912e6d4da486f14e3dfff0763d7b385af4ffce
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2015-08-27 08:33:46 +0530

    Adding license headers.

--- org.wso2.developerstudio.web.editor/src/org/wso2/developerstudio/webeditor/core/WebBasedEditor.java
+++ org.wso2.developerstudio.web.editor/src/org/wso2/developerstudio/webeditor/core/WebBasedEditor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wso2.developerstudio.webeditor.core;
 
 import org.eclipse.core.commands.operations.ObjectUndoContext;
@@ -48,14 +63,7 @@ public class WebBasedEditor extends EditorPart {
 
 	@Override
 	public void doSave(IProgressMonitor arg0) {
-//		String[] args = new String[]{"{'data' : {'_type':2}}"};
-//		BrowserScript script = ScriptFactory.createFunctionCallScript("receiveMessage", args);
-		JsonObject msg = new JsonObject();
-		JsonObject data = new JsonObject();
-		msg.add("data", data);
-		data.addProperty("_type", 2);
-		JsonObject[] args = new JsonObject[]{msg};
-		BrowserScript script = ScriptFactory.createFunctionCallScript("receiveMessage", args);
+		BrowserScript script = ScriptFactory.createFunctionCallScript("saveFile");
 		executeScriptOnBrowser(script);
 	}
 
